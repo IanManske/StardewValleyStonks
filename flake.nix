d510f36aee54bececcdedf91b82b4160b3483d90
Ian Manske <ian.manske@pm.me>
2025-10-16 21:21:38 -0700

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nix development shell for this repo.";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs { inherit system; }; in
+        {
+          devShells.default = with pkgs; mkShell {
+            buildInputs = [
+              nodejs
+              pnpm
+              pandoc
+              dotnet-sdk
+            ];
+            packages = [];
+          };
+        }
+      );
+}
